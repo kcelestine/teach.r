2fb923d6f347d94b84d5344ba17d15c4bd0e375e
Khadijah Celestine <khadijah.celestine@gmail.com>
2014-07-08 17:20:08 -0700

    sucessfully create first failing test

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -6,6 +6,12 @@ describe Course do
                          password_digest: "hunter2",
                          type: "i")}
 
+  let(:wdi) { Course.new( 
+			name: "Web Development Immersive",
+			course_code: "WDI0614",
+			start_date: "June 9, 2014",
+			end_date: "September 8, 2014" ) }
+
   it "is valid with a username, email, and password" do
     expect(hari).to be_valid
   end
