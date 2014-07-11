bc01cad4305ce348414b24d740d50872b18f6787
Khadijah Celestine <khadijah.celestine@gmail.com>
2014-07-11 14:01:31 -0700

    rewrote routes file, no more nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,6 @@
 Rails.application.routes.draw do
-  devise_scope :user do
-    root to: "courses#index"
-    match '/sessions/user', to: 'devise/sessions#create', via: :post
-  end
-  #get 'sign_in', :to => 'users/sessions#new', :as => :new_session
   devise_for :users
-  resources :users, except: [:index]
-  resources :questions, only: [:edit, :new, :update, :create, :index]
-
-  # make all routes for courses
+  resources :questions, only: [:new, :create, :edit, :update]
+  resources :sessions, only: [:new, :create, :edit, :update, :show]
   resources :courses, except: [:destroy]
-
-  resources :courses do
-    resources :sessions, only: [:edit, :new, :update, :create, :index, :show]
-  end
-
-
-  #before_filter :authenticate_user!
 end
