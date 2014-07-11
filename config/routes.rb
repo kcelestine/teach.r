6f308ce9648ec67859f6ede3b7efe88849463203
Khadijah Celestine <khadijah.celestine@gmail.com>
2014-07-10 19:03:12 -0700

    added devise to User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "courses#index"
   resources :users, except: [:index]
   resources :questions, only: [:edit, :new, :update, :create, :index]
