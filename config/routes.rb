d69f2c78c6cf7737e1941dca7d2efeb999139e8e
Khadijah Celestine <khadijah.celestine@gmail.com>
2014-07-11 12:47:23 -0700

    implemented nested routes for course and session

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
-
   devise_scope :user do
     root to: "courses#index"
     match '/sessions/user', to: 'devise/sessions#create', via: :post
   end
+
   devise_for :users
   resources :users, except: [:index]
   resources :questions, only: [:edit, :new, :update, :create, :index]
-  resources :sessions, only: [:edit, :new, :update, :create, :index, :show]
+
+  # make all routes for courses
   resources :courses, except: [:destroy]
+
+  resources :courses do
+    resources :sessions, only: [:edit, :new, :update, :create, :index, :show]
+  end
+
+
+  #before_filter :authenticate_user!
 end
