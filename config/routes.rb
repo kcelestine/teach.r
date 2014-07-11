1edd052780677a44625056c271c74a73170e98e7
Khadijah Celestine <khadijah.celestine@gmail.com>
2014-07-11 12:23:31 -0700

    user can now sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
+  devise_scope :user do
+    root to: "courses#index"
+    match '/sessions/user', to: 'devise/sessions#create', via: :post
+  end
   devise_for :users
-  root to: "courses#index"
   resources :users, except: [:index]
   resources :questions, only: [:edit, :new, :update, :create, :index]
   resources :sessions, only: [:edit, :new, :update, :create, :index, :show]
