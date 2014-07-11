1d06b772a112083655ccf924401986d4edc8a646
Khadijah Celestine <khadijah.celestine@gmail.com>
2014-07-10 21:29:26 -0700

    login and logout functionality is working but 12 tests fail. Thank GOD for testsrspecrspec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  #what is the rails 4 way to prevent password logging?
+  #rails 2
+  #filter_parameter_logging :password, :password_confirmation
+  #rails 3
+  #config.filter_parameters += [:password, :password_confirmation]
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,4 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-
-  validates_presence_of :name
-  validates_length_of :name, minimum: 8, maximum: 50
-  validate :email_regex
-
-  def email_regex
-    if email.present? && !email.match(/\A[^@]+@[^@]+\z/)
-      errors.add :email, "This is not a valid email format"
-    end
-  end
 end
