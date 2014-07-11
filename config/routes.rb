3976ea4e6afaaf20d09b3a9eae7f088bcf384d79
Khadijah Celestine <khadijah.celestine@gmail.com>
2014-07-10 19:01:27 -0700

    added a root to courses#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "courses#index"
   resources :users, except: [:index]
   resources :questions, only: [:edit, :new, :update, :create, :index]
   resources :sessions, only: [:edit, :new, :update, :create, :index, :show]
